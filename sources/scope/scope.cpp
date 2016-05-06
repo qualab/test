46f23b806f883de23f83c09dd046c0c5786dfbee
qua <qualab@gmail.com>
2016-05-06 22:33:23 +0300

    Added project "trace" from sub-module into solution "test" and added dependencies. Filled methods of test::scope::iterator.

--- sources/scope/scope.cpp
+++ sources/scope/scope.cpp
@@ -1,5 +1,6 @@
 ﻿// Implementation of test::scope methods
 
+#include <trace/call>
 #include "scope_data"
 #include <algorithm>
 
@@ -49,16 +50,129 @@ namespace test
         return m_data->get_owner();
     }
 
-    void scope::run()
+    void scope::run(const char* file, long long line)
     {
-        // TODO: trace::call::stack::push()
-        
+        using trace::call;
+        auto warden = call::stack::push(
+            call(get_full_name(), file, line)
+        );
+        each([file, line](scope& inner) {
+            inner.run(file, line);
+        });
     }
 
     scope& scope::global()
     {
         static scope global;
         return global;
+    }
+
+    void scope::each(const scope::handler& functor)
+    {
+        std::for_each(begin(), end(), functor);
+    }
+
+    scope::iterator scope::begin()
+    {
+        return m_data->begin();
+    }
+
+    scope::iterator scope::end()
+    {
+        return m_data->end();
+    }
+
+    scope::iterator scope::find(const char* name)
+    {
+        return m_data->find(name);
+    }
+
+    scope::iterator::iterator(scope::iterator::data&& new_data)
+        : m_data(new(m_buffer) scope::iterator::data(std::move(new_data)))
+    {
+    }
+
+    scope::iterator::iterator(const scope::iterator& another)
+        : m_data(new(m_buffer) scope::iterator::data(*another.m_data))
+    {
+    }
+
+    scope::iterator::iterator(scope::iterator&& temporary)
+        : m_data(new(m_buffer) scope::iterator::data(std::move(*temporary.m_data)))
+    {
+    }
+
+    scope::iterator::~iterator()
+    {
+        m_data->~data();
+    }
+
+    scope::iterator& scope::iterator::operator = (const scope::iterator& another)
+    {
+        *m_data = *another.m_data;
+        return *this;
+    }
+
+    scope::iterator& scope::iterator::operator = (scope::iterator&& temporary)
+    {
+        *m_data = std::move(*temporary.m_data);
+        return *this;
+    }
+
+    scope::iterator& scope::iterator::operator ++ ()
+    {
+        ++m_data->get_value();
+        return *this;
+    }
+
+    scope::iterator& scope::iterator::operator -- ()
+    {
+        --m_data->get_value();
+        return *this;
+    }
+
+    scope::iterator scope::iterator::operator ++ (int)
+    {
+        iterator result(*this);
+        ++m_data->get_value();
+        return result;
+    }
+
+    scope::iterator scope::iterator::operator -- (int)
+    {
+        iterator result(*this);
+        --m_data->get_value();
+        return *this;
+    }
+
+    const scope& scope::iterator::operator * () const
+    {
+        return m_data->get_value()->second;
+    }
+
+    scope& scope::iterator::operator * ()
+    {
+        return m_data->get_value()->second;
+    }
+
+    const scope* scope::iterator::operator -> () const
+    {
+        return &**this;
+    }
+
+    scope* scope::iterator::operator -> ()
+    {
+        return &**this;
+    }
+
+    bool scope::iterator::operator == (const scope::iterator& another) const
+    {
+        return m_data->get_value() == another.m_data->get_value();
+    }
+
+    bool scope::iterator::operator != (const scope::iterator& another) const
+    {
+        return m_data->get_value() != another.m_data->get_value();
     }
 }
 
--- sources/scope/scope_data.cpp
+++ sources/scope/scope_data.cpp
@@ -5,34 +5,37 @@
 
 namespace test
 {
+    namespace
+    {
+        const std::string GLOBAL_NAME = "";
+        const std::string GLOBAL_PATH = "";
+        const std::string GLOBAL_FULL_NAME = "";
+        const std::string PATH_SEPARATOR = "/";
+    }
+
     scope::data::data(scope& self)
         : m_self(self),
           m_owner(self),
-          m_name(global_name),
-          m_path(global_path),
-          m_full_name(global_name)
+          m_name(GLOBAL_NAME),
+          m_path(GLOBAL_PATH),
+          m_full_name(GLOBAL_FULL_NAME)
     {
     }
 
     scope::data::data(scope& self, scope& owner, const char* name)
-        : m_self(self), m_owner(owner), m_name(name)
+        : m_self(self),
+          m_owner(owner),
+          m_name(name)
     {
-        if (&m_self == &m_owner && m_name == global_name)
+        m_path = m_owner.get_full_name() + PATH_SEPARATOR;
+        m_full_name = m_path + m_name;
+        auto& owner_inner = m_owner.m_data->m_inner;
+        if (owner_inner.find(m_name) != owner_inner.end())
         {
-            m_path = global_path;
-            m_full_name = global_name;
+            throw std::invalid_argument("Owner scope \"" + std::string(owner.get_full_name()) +
+                                        "\" already has a scope with name \"" + m_name + '"');
         }
-        else
-        {
-            m_path = m_owner.get_path() + std::string(path_separator) + m_owner.get_name();
-            m_full_name = m_path + std::string(path_separator) + m_name;
-            auto& owner_inner = m_owner.m_data->m_inner;
-            if (owner_inner.find(m_name) != owner_inner.end())
-            {
-                throw std::invalid_argument("Owner scope \"" + std::string(owner.get_full_name()) +
-                                            "\" already has a scope with name \"" + m_name + '"');
-            }
-        }
+        owner_inner.insert(std::make_pair(m_name, m_self));
     }
 
     const char* scope::data::get_name() const
@@ -58,6 +61,41 @@ namespace test
     scope& scope::data::get_owner()
     {
         return m_owner;
+    }
+
+    scope::iterator scope::data::begin()
+    {
+        return iterator(m_inner.begin());
+    }
+
+    scope::iterator scope::data::end()
+    {
+        return iterator(m_inner.end());
+    }
+
+    scope::iterator scope::data::find(const char* name)
+    {
+        return iterator(m_inner.find(name));
+    }
+
+    scope::iterator::data::data(const scope::iterator::data::value_type& value)
+        : m_value(value)
+    {
+    }
+
+    void scope::iterator::data::set_value(const scope::iterator::data::value_type& value)
+    {
+        m_value = value;
+    }
+
+    const scope::iterator::data::value_type& scope::iterator::data::get_value() const
+    {
+        return m_value;
+    }
+
+    scope::iterator::data::value_type& scope::iterator::data::get_value()
+    {
+        return m_value;
     }
 }
 
