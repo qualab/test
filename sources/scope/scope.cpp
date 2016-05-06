ff280577ba24d7bde538b474723559ad1d90ff23
qua <qualab@gmail.com>
2016-05-06 15:30:33 +0300

    Added test::scope::get_full_name(). Changed creation of test::scope::global(). Added iterator classes.

--- sources/scope/scope.cpp
+++ sources/scope/scope.cpp
@@ -1,21 +1,22 @@
 ﻿// Implementation of test::scope methods
 
 #include "scope_data"
+#include <algorithm>
 
 namespace test
 {
     scope::scope(scope& owner, const char* name)
-        : m_data(std::make_shared<scope::data>(owner, name))
+        : m_data(std::make_shared<scope::data>(*this, owner, name))
     {
     }
 
     scope::scope(const char* name)
-        : m_data(std::make_shared<scope::data>(global(), name))
+        : m_data(std::make_shared<scope::data>(*this, global(), name))
     {
     }
 
     scope::scope()
-        : m_data(std::make_shared<scope::data>(*this, global_name))
+        : m_data(std::make_shared<scope::data>(*this))
     {
     }
 
@@ -25,23 +26,33 @@ namespace test
 
     const char* scope::get_name() const
     {
+        return m_data->get_name();
     }
 
     const char* scope::get_path() const
     {
+        return m_data->get_path();
+    }
+
+    const char* scope::get_full_name() const
+    {
+        return m_data->get_full_name();
     }
 
     const scope& scope::get_owner() const
     {
+        return m_data->get_owner();
     }
 
     scope& scope::get_owner()
     {
+        return m_data->get_owner();
     }
 
     void scope::run()
     {
         // TODO: trace::call::stack::push()
+        
     }
 
     scope& scope::global()
--- sources/scope/scope_data.cpp
+++ sources/scope/scope_data.cpp
@@ -1,9 +1,64 @@
 ﻿// Implementation of test::scope methods
 
 #include "scope_data"
+#include <stdexcept>
 
 namespace test
 {
+    scope::data::data(scope& self)
+        : m_self(self),
+          m_owner(self),
+          m_name(global_name),
+          m_path(global_path),
+          m_full_name(global_name)
+    {
+    }
+
+    scope::data::data(scope& self, scope& owner, const char* name)
+        : m_self(self), m_owner(owner), m_name(name)
+    {
+        if (&m_self == &m_owner && m_name == global_name)
+        {
+            m_path = global_path;
+            m_full_name = global_name;
+        }
+        else
+        {
+            m_path = m_owner.get_path() + std::string(path_separator) + m_owner.get_name();
+            m_full_name = m_path + std::string(path_separator) + m_name;
+            auto& owner_inner = m_owner.m_data->m_inner;
+            if (owner_inner.find(m_name) != owner_inner.end())
+            {
+                throw std::invalid_argument("Owner scope \"" + std::string(owner.get_full_name()) +
+                                            "\" already has a scope with name \"" + m_name + '"');
+            }
+        }
+    }
+
+    const char* scope::data::get_name() const
+    {
+        return m_name.c_str();
+    }
+
+    const char* scope::data::get_path() const
+    {
+        return m_path.c_str();
+    }
+
+    const char* scope::data::get_full_name() const
+    {
+        return m_path.c_str();
+    }
+
+    const scope& scope::data::get_owner() const
+    {
+        return m_owner;
+    }
+
+    scope& scope::data::get_owner()
+    {
+        return m_owner;
+    }
 }
 
 // Unicode signature: Владимир Керимов
